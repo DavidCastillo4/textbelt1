4d7d67c33fca1b0ec28a11b320337624d512669a
David Castillo <david.castillo4@gmail.com>
2024-07-22 12:55:01 -0500

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+let port = 3116;
+let express = require('express');
+let cors = require('cors');
+let app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/sms', (req, res) => {
+ // const { fromNumber, text } = req.body;
+ // let data = req.body
+ // console.log(data)
+ //console.log(`Received SMS reply from ${fromNumber}: ${text}`);
+ res.status(200).send('24422');
+});
+
+
+app.listen(port, () => { console.log(`port:${port}`); });
