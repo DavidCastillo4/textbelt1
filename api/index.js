2c0aa90c26709b21a4f48fd07acccfdf31a90dca
David Castillo <david.castillo4@gmail.com>
2024-07-23 10:17:14 -0500

    msg

--- api/index.js
+++ api/index.js
@@ -1,20 +1,21 @@
-let port = 3000;
+require('dotenv').config();
+
 let express = require('express');
 //let cors = require('cors');
 let app = express();
 // app.use(cors());
 // app.use(express.json());
-
+let port = 3000;
 
 app.get('/', (req, res) => {
  // const { fromNumber, text } = req.body;
  // let data = req.body
  // console.log(data)
  //console.log(`Received SMS reply from ${fromNumber}: ${text}`);
- res.status(200).send('2433444442');
+ res.status(200).send('1111');
 });
 
 
-app.listen(3000, () => { console.log(`port:${port}`); });
+app.listen(port, () => { console.log(`port:${port}`); });
 
 module.exports = app;
