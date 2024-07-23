ab9c1d7efb495601eb26cddcfb94d1bdcc19eb66
David Castillo <david.castillo4@gmail.com>
2024-07-23 12:10:01 -0500

    ddd

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
-let port = 3000;
+let port = 3001;
 app.get('/', (req, res) => {
- res.status(200).send('1111');
+ res.status(200).send('777777');
 });
 app.listen(port, () => { console.log(`port:${port}`); });
 module.exports = app;
